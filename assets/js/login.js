79255805543ac3b7dc07c1c6ef218e8d6eaceebc
Boris Tacyniak <dallas62@free.fr>
2014-09-27 12:46:50 +0200

    Prepare login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,25 @@
 ( function( jQuery ) {
     window.Login = ( function() {
-        var emailField,
+        var loginForm,
+            emailStep,
+            passwordStep,
+            termsStep,
+            emailField,
             passwordField,
             termsOfUseField,
-            submitField;
+            previousField,
+            nextField;
 
         function initializeFields() {
+            loginForm = jQuery( '#frm_login' );
+            emailStep = jQuery( '#step_email' );
+            passwordStep = jQuery( '#step_password' );
+            termsStep = jQuery( '#step_terms' );
             emailField = jQuery( '#login_email' );
             passwordField = jQuery( '#login_pass' );
             termsOfUseField = jQuery( '#login_terms' );
-            submitField = jQuery( '#login_submit' );
+            previousField = jQuery( '#login_prev' );
+            nextField = jQuery( '#login_next' );
         }
 
         return {
