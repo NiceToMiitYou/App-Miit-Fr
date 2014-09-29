7e3dca33adaeb9be5f0f3a6bbdbb1cea54a7e176
jordan Cortet <jord@MacBook-Pro-de-jordan.local>
2014-09-30 01:54:58 +0200

    login steps system

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,15 +5,24 @@
         function( $scope ) {
             $scope.user = {
                 mail: '',
-                password: ''
+                password: '',
+                newuser: true
             };
 
             $scope.s = 1;
 
             $scope.next = function() {
                 if($scope.s < 3) {
-                    if($scope.user.mail && !$scope.user.password) {$scope.s = 2;}
-                    if($scope.user.mail && $scope.user.password) {$scope.s = 3;}
+                    if($scope.user.mail && !$scope.user.password && $scope.s==1) {
+                        ITConnect.user.login($scope.user.mail, '', function(data) {
+                            $scope.user.newuser = !data.exist;
+                            $scope.s = 2;
+                            $scope.$apply();
+                        });
+                    }
+                    if($scope.user.mail && $scope.user.password) {
+                        $scope.s = 3;
+                    }
                     
                 }
             }
