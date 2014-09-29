c64822592e5196a57b5379690c046cd6b52a6450
jordan Cortet <jord@MacBook-Pro-de-jordan.local>
2014-09-29 23:21:23 +0200

    login steps system

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,12 +8,20 @@
                 password: ''
             };
 
+            $scope.s = 1;
+
             $scope.next = function() {
-                
+                if($scope.s < 3) {
+                    if($scope.user.mail && !$scope.user.password) {$scope.s = 2;}
+                    if($scope.user.mail && $scope.user.password) {$scope.s = 3;}
+                    
+                }
             }
 
             $scope.previous = function() {
-                
+                if($scope.s > 1) {
+                    $scope.s--;
+                }
             }
 
         } );
