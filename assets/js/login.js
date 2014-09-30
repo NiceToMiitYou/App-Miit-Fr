7d33e8fca3c0a15e8adfcc72784d4561a1213d30
jordan Cortet <jord@MacBook-Pro-de-jordan.local>
2014-09-30 18:59:27 +0200

    backend login system

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,25 +6,42 @@
             $scope.user = {
                 mail: '',
                 password: '',
+                password_c: '',
+                cgu: false,
                 newuser: true
             };
 
             $scope.s = 1;
 
             $scope.next = function() {
-                if($scope.s < 3) {
-                    if($scope.user.mail && !$scope.user.password && $scope.s==1) {
+                    if($scope.user.mail && $scope.s==1) {
                         ITConnect.user.login($scope.user.mail, '', function(data) {
                             $scope.user.newuser = !data.exist;
                             $scope.s = 2;
                             $scope.$apply();
                         });
                     }
-                    if($scope.user.mail && $scope.user.password) {
-                        $scope.s = 3;
+                    if($scope.user.password && $scope.s==2 && (!$scope.user.newuser || ($scope.user.password == $scope.user.password_c) )) {
+                        if($scope.user.newuser) {
+                            ITConnect.user.register($scope.user.mail, $scope.user.password, function(data) {
+                                console.log(data);
+                                if(data.done) {
+                                    $scope.s = 3;
+                                    $scope.$apply();
+                                }
+                            });
+                        } else if (!$scope.user.newuser) {
+                            $scope.s = 3;
+                        }
+                    }
+                    if($scope.user.mail && $scope.user.password && $scope.s==3 && $scope.user.cgu) {
+                        ITConnect.user.login($scope.user.mail, $scope.user.password, function(data) {
+                            console.log(data);
+                            if(data.done) {
+                            }
+                        });
                     }
                     
-                }
             }
 
             $scope.previous = function() {
@@ -34,5 +51,11 @@
             }
 
         } );
+
+    document.getElementById('login_email').onkeydown = function(e){
+        if (e.keyCode == 9) {
+            return false;
+        }
+    }
 
 } )();
