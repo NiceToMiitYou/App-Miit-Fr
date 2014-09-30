b6497783563dde62a08692d2646563c5da79d0ae
Boris Tacyniak <dallas62@free.fr>
2014-09-30 19:19:44 +0200

    Beautify

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,48 +14,48 @@
             $scope.s = 1;
 
             $scope.next = function() {
-                    if($scope.user.mail && $scope.s==1) {
-                        ITConnect.user.login($scope.user.mail, '', function(data) {
-                            $scope.user.newuser = !data.exist;
-                            $scope.s = 2;
-                            $scope.$apply();
-                        });
-                    }
-                    if($scope.user.password && $scope.s==2 && (!$scope.user.newuser || ($scope.user.password == $scope.user.password_c) )) {
-                        if($scope.user.newuser) {
-                            ITConnect.user.register($scope.user.mail, $scope.user.password, function(data) {
-                                console.log(data);
-                                if(data.done) {
-                                    $scope.s = 3;
-                                    $scope.$apply();
-                                }
-                            });
-                        } else if (!$scope.user.newuser) {
-                            $scope.s = 3;
-                        }
-                    }
-                    if($scope.user.mail && $scope.user.password && $scope.s==3 && $scope.user.cgu) {
-                        ITConnect.user.login($scope.user.mail, $scope.user.password, function(data) {
-                            console.log(data);
-                            if(data.done) {
+                if ( $scope.user.mail && $scope.s == 1 ) {
+                    ITConnect.user.login( $scope.user.mail, '', function( data ) {
+                        $scope.user.newuser = !data.exist;
+                        $scope.s = 2;
+                        $scope.$apply();
+                    } );
+                }
+                if ( $scope.user.password && $scope.s == 2 && ( !$scope.user.newuser || ( $scope.user.password == $scope.user.password_c ) ) ) {
+                    if ( $scope.user.newuser ) {
+                        ITConnect.user.register( $scope.user.mail, $scope.user.password, function( data ) {
+                            console.log( data );
+                            if ( data.done ) {
+                                $scope.s = 3;
+                                $scope.$apply();
                             }
-                        });
+                        } );
+                    } else if ( !$scope.user.newuser ) {
+                        $scope.s = 3;
                     }
-                    
+                }
+                if ( $scope.user.mail && $scope.user.password && $scope.s == 3 && $scope.user.cgu ) {
+                    ITConnect.user.login( $scope.user.mail, $scope.user.password, function( data ) {
+                        console.log( data );
+                        if ( data.done ) {}
+                    } );
+                }
+
             }
 
             $scope.previous = function() {
-                if($scope.s > 1) {
+                if ( $scope.s > 1 ) {
                     $scope.s--;
                 }
             }
 
         } );
 
-    document.getElementById('login_email').onkeydown = function(e){
-        if (e.keyCode == 9) {
-            return false;
-        }
+    document.getElementById( 'login_email' )
+        .onkeydown = function( e ) {
+            if ( e.keyCode == 9 ) {
+                return false;
+            }
     }
 
 } )();
