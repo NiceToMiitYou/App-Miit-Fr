c0344209680edcc94310327782a6bfbc7b5214fe
Boris Tacyniak <dallas62@free.fr>
2014-10-04 18:00:26 +0200

    Load data from the server..

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,6 @@ function init() {
     ITStorage.create( 'resources' );
 
     ITStorage.create( 'quizzes' );
-    ITStorage.create( 'questions' );
     ITStorage.create( 'presentations' );
 
     initData();
@@ -19,8 +18,10 @@ function init() {
 // Request data
 function initData() {
 
+    // Get the conference if it is not loaded
     if ( !ITStorage.db.options.get( 'conference.initialized' ) ) {
 
+        // Request the conference
         ITConnect.config.conference( function( data ) {
 
             if ( data.done ) {
@@ -32,13 +33,100 @@ function initData() {
         } );
     }
 
+    // Load data if not loaded and have to be loaded
     if ( ITStorage.db.options.get( 'user.isConnected' ) &&
-        !ITStorage.db.options.get( 'user.isLoad' ) ) {
+        !ITStorage.db.options.get( 'data.isLoaded' ) ) {
 
+        // Call initialization of data one by one and set them loaded after
+        async.waterfall(
+            [
 
+                function( callback ) {
+                    // Load chatrooms
+                    ITConnect.chatroom.list( function( data ) {
+                        if ( !data.done ) return;
 
+                        // Add each chatroom in the area
+                        for ( chatroom in data.chatrooms ) {
 
-        ITStorage.db.options.set( 'user.isLoad', true );
+                            ITStorage.db.chatrooms.set( chatroom.id, chatroom );
+                        }
+
+                        callback( null );
+                    } );
+                },
+
+                function( callback ) {
+                    // Load notes
+                    ITConnect.note.list( function( data ) {
+                        if ( !data.done ) return;
+
+                        // Add each note in the area
+                        for ( note in data.notes ) {
+
+                            ITStorage.db.notes.set( note.id, note );
+                        }
+
+                        callback( null );
+                    } );
+                },
+
+                function( callback ) {
+                    // Load resources
+                    ITConnect.resources.list( function( data ) {
+                        if ( !data.done ) return;
+
+                        // Add each resource in the area
+                        for ( resource in data.resources ) {
+
+                            ITStorage.db.resources.set( resource.id, resource );
+                        }
+
+                        callback( null );
+                    } );
+                },
+
+                function( callback ) {
+                    // Load quizzes
+                    ITConnect.questions.quizz.list( function( data ) {
+                        if ( !data.done ) return;
+
+                        var max = 0;
+                        var end = 0;
+
+                        // Add each resource in the area
+                        for ( quizz in data.quizzes ) {
+
+                            max++;
+
+                            // Get all questions of the quizz
+                            ITConnect.questions.quizz.questions(
+                                quizz.id,
+                                function( data ) {
+                                    if ( data.done ) {
+
+                                        // Add them to the current object
+                                        quizz.questions = data.questions;
+
+                                        // Store the quizz
+                                        ITStorage.db.quizzes.set( quizz.id, quizz );
+                                    }
+
+                                    end++;
+
+                                    if ( max === end ) callback( null );
+                                } );
+                        }
+                    } );
+                }
+            ],
+            function( err ) {
+                // If all right, then set isLoaded
+                if ( !err ) {
+                    ITStorage.db.options.set( 'data.isLoaded', true );
+                }
+            } );
+
     }
 }
 
