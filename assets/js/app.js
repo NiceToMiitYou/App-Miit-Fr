8e3f2f7ae15918a9f2d49611aa91ffce62ab4744
Boris Tacyniak <dallas62@free.fr>
2014-10-10 21:27:43 +0200

    add angular directives it-blur it-focus

--- assets/js/app.js
+++ assets/js/app.js
@@ -165,3 +165,48 @@ ITEventApp.filter('toArray', function() {
         return out;
     }
 });
+
+/*
+ * Use it as it-blur="(condition === true) to blur a field, like ng-show=""
+ */
+
+ITEventApp.directive('itBlur', function($timeout) {
+    return {
+        link: function(scope, element, attrs) {
+
+            scope.$watch(attrs.itBlur, function(value) {
+                if(value === true) { 
+                    
+                    $timeout(function() {
+                        element[0].blur();
+
+                        scope[attrs.itBlur] = false;
+                    });
+                }
+            });
+        }
+    };
+});
+
+/*
+ * Use it as it-focus="(condition === true) to focus a field, like ng-show=""
+ */
+
+ITEventApp.directive('itFocus', function($timeout) {
+    return {
+        link: function(scope, element, attrs) {
+
+            scope.$watch(attrs.itFocus, function(value) {
+                if(value === true) { 
+                    
+                    $timeout(function() {
+                        element[0].focus();
+
+                        scope[attrs.itFocus] = false;
+                    });
+                }
+            });
+        }
+    };
+});
+
