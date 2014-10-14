55c0c1d99a67aadc8f547bfb45dc7309048c52aa
jord14k <jord@MacBook-Pro-de-jordan.local>
2014-10-14 02:57:54 +0200

    User info save

--- assets/js/controllers/viewer.js
+++ assets/js/controllers/viewer.js
@@ -5,5 +5,19 @@ ITEventApp.controller(
             $scope.user = ITStorage.db.options.get('user');
 
             $scope.conference = ITStorage.db.options.get('conference');
+            
+            $scope.saveuser = function() {
+            	console.log("update");
+            	ITConnect.user.update(
+            		$scope.user.firstname, 
+            		$scope.user.lastname, 
+            		$scope.user.society,
+            		"",
+            		$scope.user.avatar,
+            		function (data) {
+            			console.log(data);
+            		}
+            	);
+            }
 
         } ] );
