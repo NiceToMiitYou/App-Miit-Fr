991c921dcc73cbe4dbd23efe7f67eb98327f04f7
Boris Tacyniak <dallas62@free.fr>
2014-10-15 00:14:14 +0200

    First sample of Quizz inner (no check, no submit)

--- assets/js/controllers/quizz-inner.js
+++ assets/js/controllers/quizz-inner.js
@@ -2,5 +2,16 @@ ITEventApp.controller(
     'quizzInnerController', [ '$scope', '$timeout',
         function( $scope, $timeout ) {
 
+            $scope.current = {};
 
+            function loadQuizz( quizz ) {
+                $timeout(function() {
+
+                    $scope.current = quizz;
+
+                });
+            }
+
+
+            ITStorage.db.options.bind('quizz.current', loadQuizz);
         } ] );
