6441d7d46e71edb1c8885ed41809191ff7baac96
Boris Tacyniak <dallas62@free.fr>
2014-10-16 20:53:09 +0200

    Add $scope.presentation for actual presentation

--- assets/js/controllers/viewer.js
+++ assets/js/controllers/viewer.js
@@ -12,6 +12,8 @@ ITEventApp.controller(
             $scope.conference = ITStorage.db.options.get('conference');
 
             $scope.user = ITStorage.db.options.get('user');
+
+            $scope.presentation = ITStorage.db.options.get('presentation.actual');
             
             $scope.agreeAnonyme = false;
 
@@ -24,6 +26,12 @@ ITEventApp.controller(
             function refreshUser( user ) {
                 $timeout( function() {
                     $scope.user = user;
+                } );
+            }
+
+            function refreshPresentation( presentation ) {
+                $timeout( function() {
+                    $scope.presentation = presentation;
                 } );
             }
 
@@ -69,4 +77,6 @@ ITEventApp.controller(
             $scope.saveUser = saveUser;
 
             ITStorage.db.options.bind('user', refreshUser);
+
+            ITStorage.db.options.bind('presentation.actual', refreshPresentation);
         } ] );
