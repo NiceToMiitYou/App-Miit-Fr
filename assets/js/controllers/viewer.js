c72cd7288ef33895d5dcc5c45faefcf742570728
Boris Tacyniak <dallas62@free.fr>
2014-10-16 22:48:28 +0200

    Add notifications after profile update and logout

--- assets/js/controllers/viewer.js
+++ assets/js/controllers/viewer.js
@@ -3,11 +3,10 @@ ITEventApp.controller(
         function( $scope, $timeout, $sce ) {
 
             if ( ! ITStorage.db.options.get('user.isConnected') ) {
-                ITConnect.user.logout(function( data ) {
-                    if( data.done )
-                        window.location.reload();
-                } );
+
+                logout();
             }
+
             $scope.conference = ITStorage.db.options.get('conference');
 
             $scope.user = ITStorage.db.options.get('user');
@@ -58,9 +57,26 @@ ITEventApp.controller(
                                 ITStorage.db.options.set( 'user', data.user );
 
                                 $scope.firstInit = false;
+
+                                $scope.messenger.post({
+                                    message: 'Bienvenue parmis nous!',
+                                    type: 'infos'
+                                });
+                            } else {
+
+                                $scope.messenger.post({
+                                    message: 'Une erreur c\'est produite lors de la modification de vos informations.',
+                                    type: 'error'
+                                });
                             }
                         }
                     );
+                } else {
+
+                    $scope.messenger.post({
+                        message: 'Merci de renseigner un des champs obligatoire (pseudonyme, nom ou prénom).',
+                        type: 'error'
+                    });
                 }
             }
 
@@ -74,8 +90,18 @@ ITEventApp.controller(
             }
 
             function logout() {
-            	ITConnect.user.logout();
-            	window.location.reload();
+            	
+                ITConnect.user.logout(function() {
+
+                    $scope.messenger.post({
+                        message: 'Vous venez d\'être déconnecté.',
+                        type: 'info'
+                    });
+
+                    $timeout(function() {
+                        window.location.reload();
+                    }, 1000);
+                });
             }
 
             $scope.logout = logout;
