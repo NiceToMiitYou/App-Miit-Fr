82366eda2557ffe372f184b297002cafecb08e7d
Boris Tacyniak <dallas62@free.fr>
2014-10-17 22:04:29 +0200

    Extract UserModal from main controller, and fix a bug with the displayed username

--- assets/js/controllers/viewer.js
+++ assets/js/controllers/viewer.js
@@ -9,9 +9,9 @@ ITEventApp.controller(
 
             $scope.conference = ITStorage.db.options.get('conference');
 
-            $scope.user = ITStorage.db.options.get('user');
-
             $scope.presentation = ITStorage.db.options.get('presentation.actual');
+
+            $scope.user = ITStorage.db.options.get('user');
 
             $scope.messenger = Messenger( {
                 maxMessages: 4,
@@ -22,79 +22,26 @@ ITEventApp.controller(
                     hideAfter: 2
                 }
             } );
-            
-            $scope.agreeAnonyme = false;
-
-            $scope.useUsername = false;
-
-            $scope.accountType = null;
-
-            $scope.firstInit = ! ( $scope.user.firstname || $scope.user.lastname || $scope.user.username);
-
-            $scope.safeHTML = safeHTML;
 
             function safeHTML(html) {
+
                 return $sce.trustAsHtml(html);
             }
 
             function refreshUser( user ) {
+
                 $timeout( function() {
+                
                     $scope.user = user;
                 } );
             }
 
             function refreshPresentation( presentation ) {
+                
                 $timeout( function() {
+                
                     $scope.presentation = presentation;
                 } );
-            }
-
-            function saveUser() {
-
-                if( $scope.user.firstname || $scope.user.lastname || $scope.user.username) {
-
-                    ITConnect.user.update(
-                        ( $scope.accountType === 0 ) ? $scope.user.firstname : '', 
-                        ( $scope.accountType === 0 ) ? $scope.user.lastname : '', 
-                        ( $scope.accountType === 0 ) ? $scope.user.society : '',
-                        ( $scope.accountType === 1 || $scope.useUsername ) ? $scope.user.username : '',
-                        ( $scope.accountType === 0 ) ? $scope.user.avatar : {} ,
-
-                        function (data) {
-                            if ( data.done ) {
-                                ITStorage.db.options.set( 'user', data.user );
-
-                                $scope.firstInit = false;
-
-                                $scope.messenger.post({
-                                    message: 'Bienvenue parmis nous!',
-                                    type: 'infos'
-                                });
-                            } else {
-
-                                $scope.messenger.post({
-                                    message: 'Une erreur c\'est produite lors de la modification de vos informations.',
-                                    type: 'error'
-                                });
-                            }
-                        }
-                    );
-                } else {
-
-                    $scope.messenger.post({
-                        message: 'Merci de renseigner un des champs obligatoire (pseudonyme, nom ou prénom).',
-                        type: 'error'
-                    });
-                }
-            }
-
-            function generateAnonym() {
-
-                if( $scope.accountType !== 1 ) {
-
-                    $scope.accountType = 1;
-                    $scope.user.username = 'Anonym-' + Math.floor( Math.random() * 10000000 );
-                }
             }
 
             function logout() {
@@ -107,6 +54,7 @@ ITEventApp.controller(
                     });
 
                     $timeout(function() {
+                        
                         window.location.reload();
                     }, 1000);
                 });
@@ -114,9 +62,7 @@ ITEventApp.controller(
 
             $scope.logout = logout;
             
-            $scope.generateAnonym = generateAnonym;
-
-            $scope.saveUser = saveUser;
+            $scope.safeHTML = safeHTML;
 
             ITStorage.db.options.bind('user', refreshUser);
 
