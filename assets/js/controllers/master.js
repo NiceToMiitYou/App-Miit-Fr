abcdd387b56181bcf834cbaac19a831f610f3f15
Boris Tacyniak <dallas62@free.fr>
2014-11-04 01:27:41 +0100

    Up and Down Arrow

--- assets/js/controllers/master.js
+++ assets/js/controllers/master.js
@@ -3,6 +3,9 @@ ITEventApp.controller(
         function( $scope, $timeout ) {
 
             $scope.connectedUsers = 1;
+            $scope.usersConnectedUp = true;
+            $scope.usersConnectedSame = false;
+            $scope.usersConnectedDown = false;
 
             function refreshConnectedUsers() {
 
@@ -11,11 +14,14 @@ ITEventApp.controller(
                     if( data.done ) {
 
                         $timeout(function() {
+                            $scope.usersConnectedUp = ( $scope.connectedUsers < data.users );;
+                            $scope.usersConnectedSame = ( $scope.connectedUsers === data.users );
+                            $scope.usersConnectedDown = ( $scope.connectedUsers > data.users );;
                             $scope.connectedUsers = data.users;
                         });
                     }
 
-                    setTimeout(refreshConnectedUsers, 5000);
+                    setTimeout(refreshConnectedUsers, 10000);
                 });
             }
 
