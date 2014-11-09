36abcba1ecc45d5b014f6251a85a13421c18bdef
Boris Tacyniak <boris.tacyniak@itevents.fr>
2014-11-09 13:55:52 +0100

    First release of resources (to update layout)

---
+++ assets/js/controllers/resources.js
@@ -0,0 +1,23 @@
+ITEventApp.controller(
+    'resourcesController', [ '$scope', '$timeout',
+        function( $scope, $timeout ) {
+
+            $scope.categories = {};
+
+            function loadCategories( isLoaded ) {
+
+                if( isLoaded ) {
+
+                    $timeout(function() {
+                        
+                        ITStorage.db.resources.each(function( id, category ) {
+
+                            $scope.categories[category.id] = category;
+                        } );
+                    } ); 
+                }
+            }
+
+            ITStorage.db.options.bind( 'data.isLoaded', true, loadCategories );
+
+        } ] );
