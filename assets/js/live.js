ea962436f0034c1d999e45a66199bf509a56d587
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-01-12 00:16:49 +0100

    [FIX] Fix the size of the slide, but need to center

--- assets/js/live.js
+++ assets/js/live.js
@@ -11,14 +11,19 @@ function scaleSlider() {
     var maxH, maxW;
 
     var fullscreen = $(".page-container").hasClass('fullscreen');
-    var master = $(".page-container").hasClass('master');
+    var isMaster = $(".page-container").hasClass('master');
+    var isLive = $(".page-container").hasClass('live');
 
     if(fullscreen) {
         maxH = ($(window).height()- 55) / slideH;
         maxW = $(window).width() / slideW ;
 
-    } else if(master) {
+    } else if( isMaster ) {
         maxH = ($(".page-content").height()- 290) / slideH;
+        maxW = $(".page-content").width() / slideW ;
+
+    } else if( isLive ) {
+        maxH = $(".page-content").height() / slideH;
         maxW = $(".page-content").width() / slideW ;
 
     } else {
